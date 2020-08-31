6a2097fd8c4a608caf7897e89eae5e2516d9ba11
Tobenna P. Igwe <ptigwe@gmail.com>
2020-08-31 21:49:29 +0900

    Implement distance method selection

--- dtnn/data.py
+++ dtnn/data.py
@@ -3,21 +3,32 @@ import torch
 import utils
 import pandas as pd
 import numpy as np
+from enum import Enum
 
 def gaussian_expansion(D, mu_min=-1, delta_mu=0.2, mu_max=1, sigma=0.2):
     mu = np.arange(mu_min, mu_max + delta_mu, delta_mu)
     diff = D[:,:,np.newaxis] - mu[np.newaxis, np.newaxis, :]
     return np.exp(-diff ** 2 / (2 * sigma))
 
+def get_distance_matrix(X, dist_method):
+    assert(dist_method in ('is_3D', 'euclid', 'graph'))
+
+    if dist_method == 'is_3D':
+        return X.euclid_D if X.is_3D else X.graph_D
+
+    return X[f'{dist_method}_D']
+
 class QM8Dataset(Dataset):
-    def __init__(self, target, max_atoms, mu_min=-1, delta_mu=0.2, mu_max=1, sigma=0.2, nrows=None):
-        df = pd.read_json('data/preprocessed.json', lines=True, orient='records', nrows=nrows)
+    def __init__(self, fname, target, max_atoms, mu_min=-1, delta_mu=0.2,
+                 mu_max=1, sigma=0.2, nrows=None, dist_method='euclid'):
+        df = pd.read_json(fname, lines=True, orient='records', nrows=nrows)
         self.target = torch.FloatTensor(df[target])
         Zs, Ds, sizes = [], [], []
 
         for i, x in df.iterrows():
             Zs.append(utils.pad_(torch.LongTensor(x.Z), max_atoms))
-            D_hat = gaussian_expansion(np.array(x.D), mu_min, delta_mu, mu_max, sigma)
+            D = np.array(get_distance_matrix(x, dist_method))
+            D_hat = gaussian_expansion(D, mu_min, delta_mu, mu_max, sigma)
             Ds.append(utils.pad_Dhat(torch.FloatTensor(D_hat), max_atoms))
             sizes.append(len(x.Z))
 
