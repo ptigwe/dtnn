dbb46b591f98ba80d3d2734e97b832229832a7bd
Tobenna P. Igwe <ptigwe@gmail.com>
2020-08-31 09:54:24 +0900

    Add option for limiting the number of rows

--- data.py
+++ data.py
@@ -10,14 +10,17 @@ def gaussian_expansion(D, mu_min=-1, delta_mu=0.2, mu_max=1, sigma=0.2):
     return np.exp(-diff ** 2 / (2 * sigma))
 
 class QM8Dataset(Dataset):
-    def __init__(self, target, max_atoms, num_gauss):
-        df = pd.read_json('data/preprocessed.json')
+    def __init__(self, target, max_atoms, mu_min=-1, delta_mu=0.2, mu_max=1, sigma=0.2, nrows=None):
+        df = pd.read_json('data/preprocessed.json', lines=True, orient='records', nrows=nrows)
         self.target = torch.FloatTensor(df[target])
         Zs, Ds, sizes = [], [], []
+
         for i, x in df.iterrows():
             Zs.append(utils.pad_(torch.LongTensor(x.Z), max_atoms))
-            Ds.append(utils.pad_Dhat(torch.FloatTensor(gaussian_expansion(np.array(x.D), -1, 0.2, 1)), max_atoms))
+            D_hat = gaussian_expansion(np.array(x.D), mu_min, delta_mu, mu_max, sigma)
+            Ds.append(utils.pad_Dhat(torch.FloatTensor(D_hat), max_atoms))
             sizes.append(len(x.Z))
+
         self.Zs = torch.stack(Zs)
         self.Ds = torch.stack(Ds)
         self.sizes = torch.LongTensor(sizes)
